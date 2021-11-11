546555db2a61c08e000816d33ff538bac8b1eb88
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-11-11 03:04:12 +0100

    Added range function

--- components/Chart.js
+++ components/Chart.js
@@ -2,6 +2,22 @@ import styles from '../styles/Chart.module.scss';
 import Row from '../components/Row';
 import ChartIndexes from './ChartIndexes';
 const distances = [1, 3, 10, 21, 42];
+const paceRange = {
+  paceMinMin: 3,
+  paceMinMAx: 4,
+  paceSecMin: 10,
+  paceSecMax: 40,
+};
+
+const range = (start, stop, step = 10) => {
+  let arraySec = [];
+  let i = start;
+  while (i !== stop) {
+    arraySec.push(i);
+    i += step;
+  }
+  return arraySec;
+};
 
 function Chart() {
   return (
